103202381d5fe8571d047cc8ff07f5d4235a3264
shobilo <58330458+shobilo@users.noreply.github.com>
2022-01-05 15:35:31 +0300

    Unknown page was added
    When you go to the unexisted roots, the unknown page will show up

--- src/pages/UnknownPage.jsx
+++ src/pages/UnknownPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const UnknownPage = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <p>You have visited the unexisted page</p>
+      <Link to="/">Go to the main page</Link>
+    </>
   )
 }
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import About from '../pages/About'
 import Main from '../pages/Main'
 import RepositoryIdPage from '../pages/RepositoryIdPage'
+import UnknownPage from '../pages/UnknownPage'
 
 const publicRoutes = [
   {path: '/', element: <Main/>, exact: true},
   {path: '/about', element: <About/>, exact: true},
-  {path: '/repository/:id', element: <RepositoryIdPage/>, exact: true}
+  {path: '/repository/:id', element: <RepositoryIdPage/>, exact: true},
+  {path: '*', element: <UnknownPage/>, exact: false}
 ]
 
 export default publicRoutes
